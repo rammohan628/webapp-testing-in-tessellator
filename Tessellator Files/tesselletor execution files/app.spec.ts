b852a6918aff07e414cd7074b6c0a8ab17dcede1
rammohan628 <68901233+rammohan628@users.noreply.github.com>
2024-08-12 16:27:20 +0530

    full test script

--- Tessellator Files/tesselletor execution files/app.spec.ts
+++ Tessellator Files/tesselletor execution files/app.spec.ts
@@ -1,2 +1,14 @@
 const { test, expect } = require('@playwright/test');
 const baseURL = process.env.BASE_URL;
+
+
+test('check if index.html has title !', async ({ page }) => {
+  // Navigate to the index.html page
+  await page.goto(baseURL);
+
+  // Get the title of the page
+  const title = await page.title();
+
+  // Assert the title
+  expect(title).toBe('Hello KMIT!');
+});
